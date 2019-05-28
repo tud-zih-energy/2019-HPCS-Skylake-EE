69bb32d078d824dfd99f3a828bbd016c7b3d634b
Robert Schöne <rschoene@users.noreply.github.com>
2019-05-28 16:18:30 +0200

    added license to ufs test

--- ufs-latencies/latency_test.c
+++ ufs-latencies/latency_test.c
@@ -1,3 +1,20 @@
+
+/*   Test program that triggers uncore frequency switches
+*
+*    Copyright (C) 2019  TU Dresden
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 #define _GNU_SOURCE 1
 
 #include <asm/unistd.h>
